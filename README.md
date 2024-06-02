cb56fc0e290686d4507c087cfb6b66888cb5d856
Juan Antonio Breña Moral <bren@juanantonio.info>
2024-06-02 18:41:58 +0200

    Updating the repo

--- README.md
+++ README.md
@@ -3,12 +3,24 @@
 ![](docs/exceptions.png)
 
 ```bash
+git submodule add https://github.com/openjdk/jdk6
+git submodule add https://github.com/openjdk/jdk7
 git submodule add https://github.com/openjdk/jdk8
 git submodule add https://github.com/openjdk/jdk9
 git submodule add https://github.com/openjdk/jdk10
 git submodule add https://github.com/openjdk/jdk11
 git submodule add https://github.com/openjdk/jdk12
+git submodule add https://github.com/openjdk/jdk13
+git submodule add https://github.com/openjdk/jdk14
+git submodule add https://github.com/openjdk/jdk15
+git submodule add https://github.com/openjdk/jdk16
 
+git submodule add https://github.com/openjdk/jdk17
+git submodule add https://github.com/openjdk/jdk18
+git submodule add https://github.com/openjdk/jdk19
+git submodule add https://github.com/openjdk/jdk20
+git submodule add https://github.com/openjdk/jdk21
+git submodule add https://github.com/openjdk/jdk22
 
 git submodule update --init --recursive
 git submodule update --remote
--- src/main/java/info/jab/jdk/ExceptionCounterExample.java
+++ src/main/java/info/jab/jdk/ExceptionCounterExample.java
@@ -21,6 +21,12 @@ public class ExceptionCounterExample {
 
         ExceptionCounter playlistGenerator = new ExceptionCounter();
 
+        final List<String> jdk6 = List.of("./jdk6/");
+        var jdk6Results = playlistGenerator.countExceptions(jdk6);
+
+        final List<String> jdk7 = List.of("./jdk7/");
+        var jdk7Results = playlistGenerator.countExceptions(jdk7);
+
         final List<String> jdk8 = List.of("./jdk8/");
         var jdk8Results = playlistGenerator.countExceptions(jdk8);
 
@@ -36,10 +42,59 @@ public class ExceptionCounterExample {
         final List<String> jdk12 = List.of("./jdk12/");
         var jdk12Results = playlistGenerator.countExceptions(jdk12);
 
+        final List<String> jdk13 = List.of("./jdk13/");
+        var jdk13Results = playlistGenerator.countExceptions(jdk13);
+
+        final List<String> jdk14 = List.of("./jdk14/");
+        var jdk14Results = playlistGenerator.countExceptions(jdk14);
+
+        final List<String> jdk15 = List.of("./jdk15/");
+        var jdk15Results = playlistGenerator.countExceptions(jdk15);
+
+        final List<String> jdk16 = List.of("./jdk16/");
+        var jdk16Results = playlistGenerator.countExceptions(jdk16);
+
+        /*
+        final List<String> jdk17 = List.of("./jdk17/");
+        var jdk17Results = playlistGenerator.countExceptions(jdk17);
+
+        final List<String> jdk18 = List.of("./jdk18/");
+        var jdk18Results = playlistGenerator.countExceptions(jdk18);
+
+        final List<String> jdk19 = List.of("./jdk19/");
+        var jdk19Results = playlistGenerator.countExceptions(jdk19);
+
+        final List<String> jdk20 = List.of("./jdk20/");
+        var jdk20Results = playlistGenerator.countExceptions(jdk20);
+
+        final List<String> jdk21 = List.of("./jdk21/");
+        var jdk21Results = playlistGenerator.countExceptions(jdk21);
+
+        final List<String> jdk22 = List.of("./jdk22/");
+        var jdk22Results = playlistGenerator.countExceptions(jdk22);
+
+         */
+
+        //Results
+        System.out.println(jdk6Results);
+        System.out.println(jdk7Results);
         System.out.println(jdk8Results);
         System.out.println(jdk9Results);
         System.out.println(jdk10Results);
         System.out.println(jdk11Results);
         System.out.println(jdk12Results);
+        System.out.println(jdk13Results);
+        System.out.println(jdk14Results);
+        System.out.println(jdk15Results);
+        System.out.println(jdk16Results);
+        /*
+        System.out.println(jdk17Results);
+        System.out.println(jdk18Results);
+        System.out.println(jdk19Results);
+        System.out.println(jdk20Results);
+        System.out.println(jdk21Results);
+        System.out.println(jdk22Results);
+
+         */
     }
 }
