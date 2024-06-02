d11931743eddb1dcae23a020fd42d73c0cc341c0
Juan Antonio Breña Moral <bren@juanantonio.info>
2024-06-02 19:38:47 +0200

    Initial works (#1)
    * Initial works
    * Adding submodules
    * Adding initial results

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# exceptions-in-java
+# Exceptions in Java
+
+**Checked Exceptions:**
+Checked exceptions are exceptions that are checked at compile-time. The Java compiler requires methods that can throw checked exceptions to either handle them within a try-catch block or declare them using the throws keyword in the method signature. This ensures that the programmer is aware of potential issues and has made provisions to handle them. Common examples of checked exceptions include IOException, SQLException, and ClassNotFoundException.
+
+**Unchecked Exceptions:**
+Unchecked exceptions, also known as runtime exceptions, are exceptions that are not checked at compile-time but rather at runtime. These exceptions extend the RuntimeException class. The Java compiler does not require methods to explicitly handle or declare unchecked exceptions. They typically indicate programming errors such as logic mistakes or improper use of an API. Examples include NullPointerException, ArrayIndexOutOfBoundsException, and IllegalArgumentException.
+
+![](docs/exceptions.gif)
+Source: https://web.deu.edu.tr/doc/oreily/java/langref/ch09_04.htm 
+
+![](./docs/exception-evolution.png)
+
+| JDK   | Checked exceptions | Unchecked exceptions |
+|-------|--------------------|----------------------|
+| JDK6  | 239                | 178                  |
+| JDK7  | 244                | 178                  |
+| JDK8  | 322                | 239                  |
+| JDK9  | 362                | 345                  |
+| JDK10 | 349                | 344                  |
+| JDK11 | 353                | 544                  |
+| JDK12 | 359                | 544                  |
+| JDK13 | 354                | 546                  |
+| JDK14 | 360                | 367                  |
+| JDK15 | 363                | 361                  |
+| JDK16 | 367                | 364                  |
+| JDK17 | 369                | 362                  |
+| JDK18 | 372                | 364                  |
+| JDK19 | 381                | 376                  |
+| JDK20 | 387                | 378                  |
+| JDK21 | 394                | 381                  |
+| JDK22 | 396                | 395                  |
+
+
+
+## How to build in local?
+
+```bash
+git submodule add https://github.com/openjdk/jdk6
+git submodule add https://github.com/openjdk/jdk7
+git submodule add https://github.com/openjdk/jdk8
+git submodule add https://github.com/openjdk/jdk9
+git submodule add https://github.com/openjdk/jdk10
+git submodule add https://github.com/openjdk/jdk11
+git submodule add https://github.com/openjdk/jdk12
+git submodule add https://github.com/openjdk/jdk13
+git submodule add https://github.com/openjdk/jdk14
+git submodule add https://github.com/openjdk/jdk15
+git submodule add https://github.com/openjdk/jdk16
+
+git submodule add https://github.com/openjdk/jdk17
+git submodule add https://github.com/openjdk/jdk18
+git submodule add https://github.com/openjdk/jdk19
+git submodule add https://github.com/openjdk/jdk20
+git submodule add https://github.com/openjdk/jdk21
+git submodule add https://github.com/openjdk/jdk22
+
+git submodule update --init --recursive
+git submodule update --remote
+
+./mvnw clean compile exec:java -Dexec.mainClass="info.jab.jdk.ExceptionCounterExample" -Dexec.args="--enable-preview"
+
+./mvnw prettier:write
+```
---
+++ src/main/java/info/jab/jdk/ExceptionCounterExample.java
@@ -0,0 +1,94 @@
+package info.jab.jdk;
+
+import com.github.lalyos.jfiglet.FigletFont;
+import java.io.IOException;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ExceptionCounterExample {
+
+    public static void main(String... args) {
+        Consumer<String> showHeader = param -> {
+            try {
+                String asciiArt = FigletFont.convertOneLine(param);
+                System.out.println(asciiArt);
+            } catch (IOException e) {
+                throw new RuntimeException(e.getLocalizedMessage(), e);
+            }
+        };
+
+        showHeader.accept("OpenJDK");
+
+        ExceptionCounter playlistGenerator = new ExceptionCounter();
+
+        final List<String> jdk6 = List.of("./jdk6/");
+        var jdk6Results = playlistGenerator.countExceptions(jdk6);
+
+        final List<String> jdk7 = List.of("./jdk7/");
+        var jdk7Results = playlistGenerator.countExceptions(jdk7);
+
+        final List<String> jdk8 = List.of("./jdk8/");
+        var jdk8Results = playlistGenerator.countExceptions(jdk8);
+
+        final List<String> jdk9 = List.of("./jdk9/");
+        var jdk9Results = playlistGenerator.countExceptions(jdk9);
+
+        final List<String> jdk10 = List.of("./jdk10/");
+        var jdk10Results = playlistGenerator.countExceptions(jdk10);
+
+        final List<String> jdk11 = List.of("./jdk11/");
+        var jdk11Results = playlistGenerator.countExceptions(jdk11);
+
+        final List<String> jdk12 = List.of("./jdk12/");
+        var jdk12Results = playlistGenerator.countExceptions(jdk12);
+
+        final List<String> jdk13 = List.of("./jdk13/");
+        var jdk13Results = playlistGenerator.countExceptions(jdk13);
+
+        final List<String> jdk14 = List.of("./jdk14/");
+        var jdk14Results = playlistGenerator.countExceptions(jdk14);
+
+        final List<String> jdk15 = List.of("./jdk15/");
+        var jdk15Results = playlistGenerator.countExceptions(jdk15);
+
+        final List<String> jdk16 = List.of("./jdk16/");
+        var jdk16Results = playlistGenerator.countExceptions(jdk16);
+
+        final List<String> jdk17 = List.of("./jdk17/");
+        var jdk17Results = playlistGenerator.countExceptions(jdk17);
+
+        final List<String> jdk18 = List.of("./jdk18/");
+        var jdk18Results = playlistGenerator.countExceptions(jdk18);
+
+        final List<String> jdk19 = List.of("./jdk19/");
+        var jdk19Results = playlistGenerator.countExceptions(jdk19);
+
+        final List<String> jdk20 = List.of("./jdk20/");
+        var jdk20Results = playlistGenerator.countExceptions(jdk20);
+
+        final List<String> jdk21 = List.of("./jdk21/");
+        var jdk21Results = playlistGenerator.countExceptions(jdk21);
+
+        final List<String> jdk22 = List.of("./jdk22/");
+        var jdk22Results = playlistGenerator.countExceptions(jdk22);
+
+        //Results
+        System.out.println(jdk6Results);
+        System.out.println(jdk7Results);
+        System.out.println(jdk8Results);
+        System.out.println(jdk9Results);
+        System.out.println(jdk10Results);
+        System.out.println(jdk11Results);
+        System.out.println(jdk12Results);
+        System.out.println(jdk13Results);
+        System.out.println(jdk14Results);
+        System.out.println(jdk15Results);
+        System.out.println(jdk16Results);
+        System.out.println(jdk17Results);
+        System.out.println(jdk18Results);
+        System.out.println(jdk19Results);
+        System.out.println(jdk20Results);
+        System.out.println(jdk21Results);
+        System.out.println(jdk22Results);
+    }
+}
